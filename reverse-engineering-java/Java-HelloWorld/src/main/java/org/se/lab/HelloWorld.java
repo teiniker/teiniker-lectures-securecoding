6bd1c97d70549ca7ac34071bd0304b1ea9376cd5
teiniker <egon.teiniker@gmail.com>
2017-10-20 10:27:51 +0200

    Refactoring: Added for loop.

--- reverse-engineering-java/Java-HelloWorld/src/main/java/org/se/lab/HelloWorld.java
+++ reverse-engineering-java/Java-HelloWorld/src/main/java/org/se/lab/HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld
 {
 	public static void main(String... args)
 	{
-		System.out.println("Hello KBerg!");
+		for(int i=0; i<10; i++)
+		{
+			System.out.println("Hello KBerg!");
+		}
 	}
 }
