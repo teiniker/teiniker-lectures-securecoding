b2c848abc1698a3dda47419917d2652b9275a7b2
teiniker <egon.teiniker@gmail.com>
2017-11-04 15:13:08 +0100

    Added missing implementation.

--- reverse-engineering-c/c-queue/linked_queue.c
+++ reverse-engineering-c/c-queue/linked_queue.c
@@ -2,11 +2,11 @@
 
 #include"linked_queue.h"
 
-struct queue_node *new_node(int value)
+queue_node *new_node(int value)
 {
-    queue_node *node_ptr = malloc(sizeof(queue_node));
+    queue_node *node_ptr = (queue_node *)malloc(sizeof(queue_node));
     node_ptr->value = value;
-    node_ptr->next_ptr = NULL;
+    node_ptr->prev_ptr = NULL;
     return node_ptr;
 }
 
@@ -14,30 +14,59 @@ struct queue_node *new_node(int value)
 queue *queue_init()
 {
     queue *queue_ptr = malloc(sizeof(queue));
-    queue_ptr->front = NULL;
-    queue_ptr->rear = NULL;
-    return queue;
+    queue_ptr->head_ptr = NULL;
+    queue_ptr->tail_ptr = NULL;
+    return queue_ptr;
 } 
 
 void queue_enqueue(queue *queue_ptr, int value)
 {
+    if(queue_ptr == NULL)
+        return;
+
     queue_node *new_ptr = new_node(value); 
-    if(queue_ptr->front == NULL && queue_ptr->rear == NULL)
+    if(queue_ptr->tail_ptr == NULL)
     {
-         queue_ptr->front = queue_ptr->rear = new_ptr;
+         queue_ptr->tail_ptr = queue_ptr->head_ptr = new_ptr;
     }
     else
     {
-        new_ptr->next_ptr = queue_ptr->front;
+        // queue_ptr->tail_ptr = the same
+        queue_node *tmp_ptr = queue_ptr->tail_ptr;
+        while(tmp_ptr->prev_ptr != NULL)
+        {
+            tmp_ptr = tmp_ptr->prev_ptr;
+        }
+        tmp_ptr->prev_ptr = new_ptr;
+        queue_ptr->head_ptr = new_ptr;
     }
 } 
 
 
-int queue_dequeue(queue **queue_ptr)
+int queue_dequeue(queue *queue_ptr)
 {
-    int value = 0;
-    
-
+    int value;
+    if(queue_ptr == NULL || queue_ptr->tail_ptr == NULL)
+    {
+        value = 0;
+    }
+    else
+    {
+        //  queue->head_ptr = the same
+        value = queue_ptr->tail_ptr->value;
+        queue_node *tmp_ptr = queue_ptr->tail_ptr;
+        queue_ptr->tail_ptr = tmp_ptr->prev_ptr;
+        free(tmp_ptr);
+    }
     return value;
+}
+
+void queue_dest(queue *queue_ptr)
+{
+    if(queue_ptr == NULL)
+        return;
+
+    free(queue_ptr);
+    queue_ptr = NULL;
 }
  
--- reverse-engineering-c/c-queue/linked_queue.h
+++ reverse-engineering-c/c-queue/linked_queue.h
@@ -8,18 +8,18 @@
 typedef struct node
 {
     int value;
-    struct node *next_ptr;
+    struct node *prev_ptr;
 }queue_node;
 
 typedef struct queue_
 {
-    queue_node *front;
-    queue_node *rear;
+    queue_node *head_ptr;
+    queue_node *tail_ptr;
 } queue;
 
 extern queue* queue_init();
 extern void queue_enqueue(queue *queue_ptr, int value);
-extern int queu_dequeue(queue *queue_ptr);
-
+extern int queue_dequeue(queue *queue_ptr);
+extern void queue_dest(queue *queue_ptr);
 
 #endif /*_LINKED_QUEUE_H_ */
