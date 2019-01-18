8dcd8c662cee66ce18d998fc6584e9916b7ca376
teiniker <egon.teiniker@gmail.com>
2019-01-18 12:19:46 +0100

    Removed examples.

--- secure-coding/Concurrency/Concurrency-Iterator-Executor/src/main/java/org/se/lab/AlgorithmMD5Encoder.java
+++
@@ -1,9 +0,0 @@
-package org.se.lab;
-
-public class AlgorithmMD5Encoder extends Algorithm
-{
-	protected String modifyElement(String s)
-	{
-		return MD5Encoder.convertToMD5String(s);
-	}
-}
--- secure-coding/Concurrency/Concurrency-Iterator-Executor/src/main/java/org/se/lab/AlgorithmToUpper.java
+++
@@ -1,9 +0,0 @@
-package org.se.lab;
-
-public class AlgorithmToUpper extends Algorithm
-{
-	protected String modifyElement(String s)
-	{
-		return s.toUpperCase();
-	}
-}
