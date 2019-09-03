ea98226f63ca358b18ca10f99c6b0bf63bbb7fa7
teiniker <egon.teiniker@gmail.com>
2019-09-03 11:57:11 +0200

    Refactoring.

---
+++ reverse-engineering-java/Java-ByteOrder/src/main/java/org/se/lab/ByteOrder.java
@@ -0,0 +1,14 @@
+package org.se.lab;
+
+public class HelloWorld
+{
+	int i = 0x11223344;
+
+	public static void main(String... args)
+	{
+		for(int i=0; i<10; i++)
+		{
+			System.out.println("Hello World!");
+		}
+	}
+}
