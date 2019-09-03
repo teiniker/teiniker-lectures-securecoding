64becef8ac036e933a55ed370f58cedda065360c
teiniker <egon.teiniker@gmail.com>
2019-09-03 12:32:22 +0200

    Refactoring.

--- secure-coding/DataValidation/Encoding-OWASP-Encoder/src/test/java/org/se/lab/EncoderTest.java
+++ secure-coding/DataValidation/Encoding-OWASP-Encoder/src/test/java/org/se/lab/EncoderTest.java
@@ -18,7 +18,7 @@ public class EncoderTest
 // https://www.owasp.org/index.php/OWASP_Java_Encoder_Project#tab=Use_the_Java_Encoder_Project	
 // public static String forHtmlAttribute(String input)
 // public static String forCssString(String input) 
-//  public static String forCssUrl(String input)
+// public static String forCssUrl(String input)
 // public static String forUriComponent(String input)
 // public static String forJavaScript(String input)
 // public static String forJavaScriptAttribute(String input) 
