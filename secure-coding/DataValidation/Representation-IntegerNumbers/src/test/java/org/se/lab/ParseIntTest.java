ffcbf8078841e9152be455e68cd605758a9d95fd
teiniker <egon.teiniker@gmail.com>
2020-01-09 15:32:34 +0100

    Refactoring.

---
+++ secure-coding/DataValidation/Representation-IntegerNumbers/src/test/java/org/se/lab/ParseIntTest.java
@@ -0,0 +1,34 @@
+package org.se.lab;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class IntegerParseIntTest
+{
+	@Test
+	public void testParseInt()
+	{
+		String input = "-31415";		
+		int value = Integer.parseInt(input);
+		
+		assertEquals(-31415, value);
+	}
+
+	@Test
+	public void testValueOf()
+	{
+		String input = "-31415";
+		Integer value = Integer.valueOf(input);
+
+		assertEquals(-31415, value.intValue());
+	}
+
+
+	@Test(expected=NumberFormatException.class)
+	public void testInvalidIntegerValue()
+	{
+		String input = "3.14";		
+		Integer.parseInt(input);
+	}
+}
