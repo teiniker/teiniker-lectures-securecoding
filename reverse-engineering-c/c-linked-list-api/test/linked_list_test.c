af7448d2504a8aa467aed6433633a44cd799f014
teiniker <egon.teiniker@gmail.com>
2020-10-29 16:04:37 +0100

    Refactoring.

--- reverse-engineering-c/c-linked-list-api/test/linked_list_test.c
+++ reverse-engineering-c/c-linked-list-api/test/linked_list_test.c
@@ -2,43 +2,45 @@
 #include <stdio.h>
 #include <linked_list.h>
 
+struct node *list_ptr = NULL;
+
 void setUp(void)
 {
-	// Setup
-}
-
-void tearDown(void)
-{
-	// tear down code
-}
-
-void test_LinkedList(void)
- {
     // setup linked list
-    struct node *list_ptr = NULL;
     list_ptr = list_append(list_ptr, 1);
     list_ptr = list_append(list_ptr, 2);
     list_ptr = list_append(list_ptr, 3);
     list_ptr = list_prepend(list_ptr, 4);
- 
-     // navigate
-     int length = list_length(list_ptr);
-     printf("list.length = %d\n", length);
-     for(int i=0; i<length;i++)
-     {
-         printf("list.get(%d) = %d\n",i,list_get(list_ptr,i));
-     }
- 
-     list_remove_all(list_ptr);
-    list_ptr = NULL;  //!!!
-    printf("list.length = %d\n", list_length(list_ptr));
- 
+}
+
+void tearDown(void)
+{
+    list_remove_all(list_ptr);
+    list_ptr = NULL;
+}
+
+void test_length(void)
+ {
+    // exercise
+    int length = list_length(list_ptr);
+
+    // verify
+    TEST_ASSERT_EQUAL(4, length);
+}
+
+void test_elements(void)
+ {
+	// Exercise & Verify
+	TEST_ASSERT_EQUAL(4, list_get(list_ptr,0));
+	TEST_ASSERT_EQUAL(1, list_get(list_ptr,1));
+	TEST_ASSERT_EQUAL(2, list_get(list_ptr,2));
+	TEST_ASSERT_EQUAL(3, list_get(list_ptr,3));
 }
 
 int main(void)
 {
 	UNITY_BEGIN();
-	RUN_TEST(test_LinkedList);
-
+	RUN_TEST(test_length);
+    RUN_TEST(test_elements);
 	return UNITY_END();
 }
