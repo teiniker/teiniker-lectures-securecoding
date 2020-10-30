b06b9387762c403d7b056894c09af7661309b69f
teiniker <egon.teiniker@gmail.com>
2020-10-30 12:21:49 +0100

    Added tests.

---
+++ reverse-engineering-c/c-stack-exercise/test/stack_test.c
@@ -0,0 +1,50 @@
+#include <unity.h>
+#include <stdio.h>
+#include <stack.h>
+
+node *stack_ptr = NULL;
+
+void setUp(void)
+{
+    stack_push(&stack_ptr, 1);
+    stack_push(&stack_ptr, 2);
+    stack_push(&stack_ptr, 3);
+    stack_push(&stack_ptr, 4);
+}
+
+void tearDown(void)
+{
+}
+
+
+void test_size(void)
+{
+    // Exercise
+    int size = stack_size(&stack_ptr);
+
+    // Verify
+    TEST_ASSERT_EQUAL(4, size);
+
+    // Teardown
+    stack_pop(&stack_ptr);
+    stack_pop(&stack_ptr);
+    stack_pop(&stack_ptr);
+    stack_pop(&stack_ptr);
+}
+
+void test_elements(void)
+{
+    // Exercise & Verify
+    TEST_ASSERT_EQUAL(4, stack_pop(&stack_ptr));
+    TEST_ASSERT_EQUAL(3, stack_pop(&stack_ptr));
+    TEST_ASSERT_EQUAL(2, stack_pop(&stack_ptr));
+    TEST_ASSERT_EQUAL(1, stack_pop(&stack_ptr));
+}
+
+int main(void)
+{
+	UNITY_BEGIN();
+	RUN_TEST(test_size);
+	RUN_TEST(test_elements);
+ 	return UNITY_END();
+}
--- reverse-engineering-c/c-stack/src/stack.c
+++ reverse-engineering-c/c-stack/src/stack.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "stack.h"
+#include <stack.h>
 
 node *new_node(int value)
 {
