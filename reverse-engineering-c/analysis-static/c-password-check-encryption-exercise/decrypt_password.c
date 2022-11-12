8c061e13abc683e361efe5c38bcfd5ba6deaa535
Egon Teiniker <egon.teiniker@gmail.com>
2022-11-12 12:42:13 +0100

    Refactoring.

---
+++ reverse-engineering-c/analysis-static/c-password-check-encryption-exercise/decrypt_password.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+
+int main()
+{
+    char password[256];
+    strcpy(password, "TODO");
+    printf("encrypted password: %s\n", password);
+
+    // TODO
+
+    printf("plaintext password: %s\n", password);
+
+	return 0;
+}
