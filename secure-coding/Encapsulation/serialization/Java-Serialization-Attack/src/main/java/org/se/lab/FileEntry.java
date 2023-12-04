c83c11663ee8ab31e8fcdb9a61fbd0f7cd05459b
teiniker <egon.teiniker@fh-joanneum.at>
2023-12-04 20:23:30 +0100

    Initial import.

---
+++ secure-coding/Encapsulation/serialization/Java-Serialization-Attack/src/main/java/org/se/lab/FileEntry.java
@@ -0,0 +1,80 @@
+package org.se.lab;
+
+import org.apache.commons.codec.binary.Hex;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+
+public class FileEntry implements Serializable
+{
+    private static final long serialVersionUID = 1L;
+
+
+    /*
+     * Constructor
+     */
+    public FileEntry(String path, byte[] content)
+    {
+        setPath(path);
+        setContent(content);
+    }
+
+    /*
+     * Custom serialization
+     */
+    private void readObject(ObjectInputStream in)
+            throws IOException, ClassNotFoundException
+    {
+        System.out.println("readObject()");
+        in.defaultReadObject();
+
+        FileOutputStream file = new FileOutputStream(getPath());
+        file.write(getContent());
+        file.close();
+    }
+
+
+    /*
+     * Property: content
+     */
+    private byte[] content;
+
+    public byte[] getContent()
+    {
+        return content;
+    }
+
+    public void setContent(byte[] content)
+    {
+        if (content == null)
+            throw new IllegalArgumentException("Content array is null!");
+
+        this.content = content;
+    }
+
+    /*
+     * Property: path
+     */
+    private String path;
+
+    public String getPath()
+    {
+        return path;
+    }
+
+    public void setPath(String path)
+    {
+        if (path == null)
+            throw new IllegalArgumentException("Path value is null!");
+
+        this.path = path;
+    }
+
+    @Override
+    public String toString()
+    {
+        return getPath() + ":" + Hex.encodeHexString(getContent());
+    }
+}
